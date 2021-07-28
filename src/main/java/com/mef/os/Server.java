40a15fa7d8f004550489edf6255828d7ee277cf7
Mike Francis <61028320+myriadmikef@users.noreply.github.com>
2021-07-28 10:18:14 -0600

    Initiali checkin

---
+++ src/main/java/com/mef/os/Server.java
@@ -0,0 +1,12 @@
+package com.mef.os;
+
+import io.javalin.Javalin;
+
+public class Server
+{
+    public static void main( String[] args )
+    {
+        Javalin app = Javalin.create().start( 7000 );
+        app.get( "/", ctx -> ctx.result( "Hello World" ) );
+    }
+}
