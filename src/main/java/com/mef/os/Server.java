ee6209f6da8ca26552cac1620b9b995ff563612d
Mike Francis <61028320+myriadmikef@users.noreply.github.com>
2021-07-28 11:18:36 -0600

    More

--- src/main/java/com/mef/os/Server.java
+++ src/main/java/com/mef/os/Server.java
@@ -6,7 +6,7 @@ public class Server
 {
     public static void main( String[] args )
     {
-        Javalin app = Javalin.create().start( 7000 );
+        Javalin app = Javalin.create().start( 8080 );
         app.get( "/", ctx -> ctx.result( "Hello World" ) );
     }
 }
