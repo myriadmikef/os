de1fb5aa8da16e69921a38de742e8c6052a23238
Mike Francis <61028320+myriadmikef@users.noreply.github.com>
2021-07-30 16:04:22 -0600

    More

--- src/main/java/com/mef/os/Server.java
+++ src/main/java/com/mef/os/Server.java
@@ -1,8 +1,10 @@
 package com.mef.os;
 
+import java.net.URL;
 import java.util.stream.Collectors;
 
 import io.javalin.Javalin;
+import io.javalin.http.Context;
 
 public class Server
 {
@@ -13,9 +15,28 @@ public class Server
         app.get( "/args", ctx -> ctx.result( args.length > 0 ? args[0] : "None" ) );
         app.get( "/cp", ctx -> ctx.result( System.getProperties().entrySet().stream()
             .map( e -> e.getKey() + "=" + e.getValue() + "\n" ).collect( Collectors.toList() ).toString() ) );
+        app.get( "/res", ctx -> Server.db( ctx ) );
 
         app.config.addStaticFiles( "/os/web" );
 
         // /deployments/os.jar
+    }
+
+    private static void db( Context ctx )
+    {
+        try
+        {
+            System.out.println( "Executing db" );
+            URL resource = Thread.currentThread().getContextClassLoader().getResource( "db.properties" );
+            if ( resource != null )
+            {
+                ctx.result( resource.toString() );
+            }
+            else
+                ctx.result( "Resource not found" );
+        } catch ( Exception ex )
+        {
+            ex.printStackTrace();
+        }
     }
 }
